d9f4ba580994593bd1d1df0c07c51c317e843da6
KhanLewis <khanlewis@hotmail.co.uk>
2023-03-03 10:25:02 +0000

    added booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    people = models.IntegerField()
+    special_requirements = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
