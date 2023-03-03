9ee9601e34d5846246f38900695ffadcf6238537
KhanLewis <khanlewis@hotmail.co.uk>
2023-03-03 12:39:57 +0000

    added superuser and changes to admin site in admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Booking
 # Register your models here.
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+    search_fields = ['name', 'date']
+    list_filter = ('name', 'date', 'time')
