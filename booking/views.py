0d12638016516446b37c21654dfa35c8d3a45990
KhanLewis <khanlewis@hotmail.co.uk>
2023-03-03 16:27:04 +0000

    added base.html template

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Booking
 # Create your views here.
+
+
+class HomepageView(generic.ListView):
+    model = Booking
+    template_name = "index.html"
+
